9414a2e65d373c7b2ebed809c99e80bbc46fdf9e
Julien Mialon <julien.mialon@geniussports.com>
2019-02-28 19:24:26 +0100

    wip

--- HashCode/Process.cs
+++ HashCode/Process.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HashCode
 {
@@ -8,9 +10,69 @@ namespace HashCode
         {
             List<Slide> slides = new List<Slide>();
 
+            HashSet<Picture> horizontal = pictures.Where(x => x.Orientation == Orientation.Horizontal).ToHashSet();
+            HashSet<Picture> vertical = pictures.Where(x => x.Orientation == Orientation.Vertical).ToHashSet();
+
+            Picture first = horizontal.First();
+            horizontal.Remove(first);
+            Slide slide = new Slide(first);
+            slides.Add(slide);
+
+            while (horizontal.Count > 0 || vertical.Count > 0)
+            {
                 
+            }
             
             return slides;
+        }
+
+        public static int Score(HashSet<string> left, HashSet<string> right)
+        {
+            int leftNotRight = 0;
+            int leftAndRight = 0;
+            int rightNotLeft = 0;
+            
+            foreach (string s in left)
+            {
+                if (right.Contains(s))
+                {
+                    leftAndRight++;
+                }
+                else
+                {
+                    leftNotRight++;
+                }
+            }
+
+            rightNotLeft = right.Count - leftAndRight;
+
+            return Math.Min(leftNotRight, Math.Min(leftAndRight, rightNotLeft));
+        }
+        
+        public static int Score(HashSet<string> left, HashSet<string> right1, HashSet<string> right2)
+        {
+            int leftNotRight = 0;
+            int leftAndRight = 0;a
+            int rightNotLeft = 0;
+            
+            foreach (string s in left)
+            {
+                if (right1.Contains(s) || right2.Contains(s))
+                {
+                    leftAndRight++;
+                }
+                else
+                {
+                    leftNotRight++;
+                }
+            }
+
+            int unionCount = right1.Count(right2.Contains);
+            
+            
+            rightNotLeft = right1.Count + right2.Count - leftAndRight - unionCount;
+
+            return Math.Min(leftNotRight, Math.Min(leftAndRight, rightNotLeft));
         }
     }
 }
